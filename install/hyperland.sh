50eecff57ac34805728aa81f5406ec6edba3ffe8
merlos <merlos@users.noreply.github.com>
2025-07-12 10:49:58 +0000

    add ttf-font-awesome so waybar looks ok

--- install/hyperland.sh
+++ install/hyperland.sh
@@ -1,10 +1,9 @@
 pacman -S --noconfirm --needed \
   kitty \
   hyprland hyprshot hyprpicker hyprlock hypridle  \
-  wofi waybar mako swaybg \
+  wofi waybar mako swaybg ttf-font-awesome\
   #polkit-gnome hyprland-qtutils \
-  
-  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+  #xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
 # Start Hyprland on first session
 #echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
