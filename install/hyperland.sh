1a1fd5bead701c015264c313893f63aa34ea8b43
merlos <merlos@users.noreply.github.com>
2025-07-12 08:56:54 +0000

    install hyperland

---
+++ install/hyperland.sh
@@ -0,0 +1,10 @@
+pacman -S --noconfirm --needed \
+  kitty \
+  hyprland hyprshot hyprpicker hyprlock hypridle  \
+  wofi waybar mako swaybg \
+  #polkit-gnome hyprland-qtutils \
+  
+  xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
+
+# Start Hyprland on first session
+#echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
